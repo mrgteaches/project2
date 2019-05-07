2c42484c922bdd35531267e75b22163b51fa03f6
Aaron L <aaronlng05@gmail.com>
2019-05-07 06:22:31 -0400

    started adding functionality to modal

--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var inputData = sequelize.define("inputData", {
+    question: DataTypes.STRING,
+    answer: DataTypes.TEXT
   });
-  return Example;
+  return inputData;
 };
