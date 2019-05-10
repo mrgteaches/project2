c67117e0c5c9c166ae5d7f9276d59439883c4635
Aaron L <aaronlng05@gmail.com>
2019-05-09 21:09:06 -0400

    multiple instances of data working

--- config/routes.js
+++ config/routes.js
@@ -2,27 +2,42 @@ var db = require("../models");
 
 module.exports = function (app) {
 
-    //Routers based on the method
-    app.get('/', function(req,res){
-      res.render("index")
-    });
+  //Routers based on the method
+  app.get('/', function (req, res) {
+    res.render("index")
+  });
 
-    app.get("/flashcards", function(req, res) {
-      res.render("flashcard")
-      });
+  app.get("/flashcards", function (req, res) {
+    // db.Terms.findAll({}).then
+    res.render("flashcard")
 
-    app.get("/hangman", function(req,res) {
-      res.render("hangman")
-    });
+  });
 
-    app.post("/", function(req, res) {
-      console.log(req.body);
+  app.get("/hangman", function (req, res) {
+    res.render("hangman")
+  });
+
+  app.post("/", function (req, res) {
+    console.log(req.body);
+
+    if (typeof (req.body.term) === "object") {
+      for (var i = 0; i < req.body.term.length; i++) {
+        db.Terms.create({
+          term: req.body.term[i],
+          definition: req.body.definition[i],
+        });
+        // .then(function(data) {
+        // });
+      }
+      res.render("index");
+    } else {
       db.Terms.create({
         term: req.body.term,
         definition: req.body.definition,
       })
-        .then(function(data) {
+        .then(function (data) {
           res.render("index");
         });
-    });
+    }
+  });
 };
